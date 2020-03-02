ce14b3e0877dde19251d25f73a80cb11e5ca42b2
Nguyenth306 <nguyenth306@gmail.com>
2020-03-02 01:46:21 -0600

    second version editing

--- MakePalindrome.java
+++ MakePalindrome.java
@@ -5,11 +5,19 @@ public class MakePalindrome{
       Scanner kbd = new Scanner(System.in);
       System.out.print("Enter you string: ");
       String s = kbd.nextLine();
-      System.out.println("Palindrome string is: ")
+      System.out.println("Palindrome string is: " + s);
         
    }
-   public static String reverseString(String S){
+   public static String reverseString(String s){
+       String [] list = s.split(" ");
+      for(int i = list.length - 1; i > 0; i--){
+         s = s + list[i] + " ";
+      }
+      s = s + list[0];
       
+      return("");
+      
+
    
    }
 }
